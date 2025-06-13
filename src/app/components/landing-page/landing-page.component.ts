dc728b99255eec63c7e474c9b43ebd2522b622db
tharikthajudeen0 <tharikthajudeen00@gmail.com>
2025-06-13 17:38:35 +0530

    improvement

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+   images = [
+    'assets/images/NineArchBridge.jpg',
+    'assets/images/Sigiriya.jpg',
+    'assets/images/SriDaladaMaligawa.webp',
+    'assets/images/LotusTower.webp'
+  ];
+
+  currentIndex = 0;
+  slideInterval?: any;
+
+  ngOnInit() {
+    this.slideInterval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 4000); // Change slide every 4 seconds
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.slideInterval);
+  }
 
 }
